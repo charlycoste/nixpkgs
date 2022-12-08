9aa260f6dc71ca7d029bebefdc55bb7c4fe70d05
Charles-Édouard Coste <charles-edouard@coste.dev>
2022-12-09 00:37:43 +0100

    Add ldflags

--- pkgs/development/tools/symfony-cli/default.nix
+++ pkgs/development/tools/symfony-cli/default.nix
@@ -12,6 +12,12 @@ buildGoModule rec {
     sha256 = "sha256-GAsyI8I+tHFMV/LqwPx2ph+w3zaqKSn9vieVQcuO+y0=";
   };
 
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.version=${version}"
+  ];
+
   postInstall = ''
     mv $out/bin/symfony-cli $out/bin/symfony
   '';
